cd31526603723fcc43013be498abbfba9597acdd
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-09-11 22:04:10 +0200

    Add some & choice

--- include/combinators.hpp
+++ include/combinators.hpp
@@ -31,6 +31,29 @@ namespace gat::combinators {
         }
     };
 
+    template<auto p>
+    constexpr inline parser<std::vector<result_type_t<p>>> some = [](std::string_view sv) noexcept -> result<std::vector<result_type_t<p>>> {
+        auto r = p(sv);
+        if(!r)
+            return {};
+        std::vector<result_type_t<p>> res{r};
+        for(;;) {
+            r = p(sv = r.remaining);
+            if(!r)
+                return {sv, std::move(res)};
+            res.push_back(std::move(r.value));
+        }
+    };
+
+    template<auto p, auto... ps>
+    constexpr inline parser<result_type_t<p>> choice = [](std::string_view sv) noexcept -> result<result_type_t<p>> {
+        static_assert(sizeof...(ps));
+        auto res = p(sv);
+        if(!res)
+            ((res = ps(sv)) || ...);
+        return res;
+    };
+
 }
 
 #endif // _GAT_COMBINATORS_HPP_
