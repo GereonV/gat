972df4eb39a7ae7f8693c0bc4c1b2ec86781e43c
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-09-11 20:59:50 +0200

    Change types for backtracking

--- include/gat.hpp
+++ include/gat.hpp
@@ -2,20 +2,36 @@
 #define _GAT_GAT_HPP_
 
 #include <string_view>
-#include <vector>
 
 namespace gat {
 
     // Result a = (a, StringView)
     template<typename ResultType>
     struct result {
+        result() = default;
+        template<typename R>
+        constexpr result(std::string_view sv, R && res) noexcept
+            : remaining{sv}, value{static_cast<R &&>(res)}, error{} {}
+        constexpr result(std::string_view sv, ResultType const & res) noexcept
+            : remaining{sv}, value{res}, error{} {}
+        constexpr operator bool() const noexcept { return !error; }
         std::string_view remaining;
-        ResultType result;
+        ResultType value;
+        bool error{true};
     };
 
-    // Parser a = StringView -> [Result a]
+    // Parser a = StringView -> Result a
     template<typename ResultType>
-    using parser = std::vector<result<ResultType>>(*)(std::string_view) noexcept;
+    using parser = result<ResultType>(*)(std::string_view) noexcept;
+
+    template<auto p>
+    using result_type_t = decltype(p({}).result);
+
+    template<auto p>
+    using result_t = result<result_type_t<p>>;
+
+    template<auto p>
+    using parser_t = parser<result_type_t<p>>;
 
     template<decltype(sizeof(char)) N>
     struct literal {
