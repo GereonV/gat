11d011d40b7a194439b19b6fb369470de8873ac0
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-09-11 21:32:39 +0200

    Update testing to new types

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,37 @@
 #include <iostream>
 #include "chars.hpp"
+#include "combinators.hpp"
 
 constexpr void chars() noexcept {
     using namespace gat::chars;
-    static_assert(!end("").empty());
-    static_assert(one("").empty());
-    static_assert(bdigit("").empty());
-    static_assert(!bdigit("0").empty());
-    static_assert(!bdigit("1").empty());
-    static_assert(bdigit("2").empty());
-    static_assert(!letter("a").empty());
-    static_assert(!letter("z").empty());
-    static_assert(!exact<'/'>("/").empty());
-    static_assert(exact<'/'>("\\/").empty());
-    static_assert(!set<'/', '%'>("/\\").empty());
-    static_assert(!set<'/', '%'>("%\\").empty());
-    static_assert(set<'/', '%'>("\\/").empty());
-    static_assert(!whitespace(" ").empty());
-    static_assert(!whitespace("\t").empty());
-    static_assert(inv<word>("_").empty());
-    static_assert(!inv<word>("|").empty());
-    static_assert(range<'0', '9'>("").empty());
-    static_assert(!range<'0', '9'>("0").empty());
-    static_assert(string<"test">("Hello World").empty());
-    static_assert(!string<"test">("test").empty());
+    static_assert(end(""));
+    static_assert(!one(""));
+    static_assert(!bdigit(""));
+    static_assert(bdigit("0"));
+    static_assert(bdigit("1"));
+    static_assert(!bdigit("2"));
+    static_assert(letter("a"));
+    static_assert(letter("z"));
+    static_assert(exact<'/'>("/"));
+    static_assert(!exact<'/'>("\\/"));
+    static_assert(set<'/', '%'>("/\\"));
+    static_assert(set<'/', '%'>("%\\"));
+    static_assert(!set<'/', '%'>("\\/"));
+    static_assert(whitespace(" "));
+    static_assert(whitespace("\t"));
+    static_assert(!whitespace("x"));
+    static_assert(!inv<word>("_"));
+    static_assert(inv<word>("|"));
+    static_assert(!range<'0', '9'>(""));
+    static_assert(range<'0', '9'>("0"));
+    static_assert(!range<'0', '9'>("a"));
+    static_assert(!string<"test">("Hello World"));
+    static_assert(string<"test">("test"));
 }
 
 int main() {
     chars();
+    static_assert(gat::combinators::many<gat::chars::one>("").value.empty());
+    static_assert(gat::combinators::many<gat::chars::one>("abc").value.size() == 3);
     std::cout << "Hello World!\n";
 }
