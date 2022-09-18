3db3527d567a2179d00082a97598a6a267c2e486
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-09-18 19:20:44 +0200

    Remove JSON due to incompleteness

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include "chars.hpp"
 #include "combinators.hpp"
 
@@ -80,121 +79,8 @@ constexpr void combinators() noexcept {
     static_assert(between<1, 3, gat::chars::letter>("abcde").value.size() == 3);
 }
 
-constexpr void json() noexcept {
-    using gat::parser;
-    using namespace gat::combinators;
-    struct json_value {
-        constexpr json_value() noexcept : tag{} {}
-        constexpr json_value(bool b) noexcept : b{b}, tag{1} {}
-        constexpr json_value(int i) noexcept : i{i}, tag{2} {}
-        constexpr json_value(std::string_view s) noexcept : tag{3} { std::construct_at(&this->s, s); }
-        constexpr json_value(std::vector<json_value> a) noexcept : a{a}, tag{4} {}
-        constexpr json_value(std::vector<std::pair<std::string, json_value>> o) noexcept : o{o}, tag{5} {}
-        constexpr json_value(json_value const & other) noexcept : tag{} { *this = other; }
-        constexpr json_value(json_value && other) noexcept : tag{} { *this = std::move(other); }
-        constexpr ~json_value() { free(); }
-        constexpr json_value & operator=(json_value const & other) noexcept {
-            free();
-            tag = other.tag;
-            switch(tag) {
-            case 1:
-                b = other.b;
-                break;
-            case 2:
-                i = other.i;
-                break;
-            case 3:
-                std::construct_at(&s, other.s);
-                break;
-            case 4:
-                a = other.a;
-                break;
-            case 5:
-                o = other.o;
-            }
-            return *this;
-        }
-
-        constexpr json_value & operator=(json_value && other) noexcept {
-            if(this == &other)
-                return *this;
-            free();
-            tag = other.tag;
-            switch(tag) {
-            case 1:
-                b = other.b;
-                break;
-            case 2:
-                i = other.i;
-                break;
-            case 3:
-                std::construct_at(&s, std::move(other.s));
-                other.tag = 0;
-                break;
-            case 4:
-                a = std::move(other.a);
-                other.tag = 0;
-                break;
-            case 5:
-                o = std::move(other.o);
-                other.tag = 0;
-            }
-            return *this;
-        }
-
-        constexpr void free() noexcept {
-            switch(tag) {
-            case 3:
-                s.~basic_string();
-                break;
-            case 4:
-                a.~vector();
-                break;
-            case 5:
-                o.~vector();
-            }
-        }
-
-        union {
-            bool b;
-            int i;
-            std::string s;
-            std::vector<json_value> a;
-            std::vector<std::pair<std::string, json_value>> o;
-        };
-        unsigned char tag;
-    };
-    constexpr parser<json_value> jsonNull{map<[](auto &&) { return json_value{}; }, gat::string<"null">>};
-    constexpr parser<json_value> jsonBool{map<[](auto && s) { return json_value{s == "true"}; }, choice<gat::string<"true">, gat::string<"false">>>};
-    constexpr parser<json_value> jsonNumber{map<[](auto && v) {
-        int i{};
-        for(auto c : v)
-            i = 10 * i + c - '0';
-        return json_value{i};
-    }, min<1, gat::chars::digit>>};
-    constexpr parser<json_value> jsonString{map<[](auto && v) {
-        return json_value{std::string_view{v.data(), v.size()}};
-    }, right<gat::chars::exact<'"'>, ahead<min<0, gat::chars::one>, gat::chars::exact<'"'>>>>};
-    
-    static_assert(!jsonNull(""));
-    static_assert(jsonNull("null"));
-    static_assert(!jsonBool("test"));
-    static_assert(!jsonBool("lol"));
-    static_assert(jsonBool("truelol"));
-    static_assert(jsonBool("truelol").value.b);
-    static_assert(jsonBool("falsexD"));
-    static_assert(!jsonBool("falsexD").value.b);
-    static_assert(!jsonNumber(""));
-    static_assert(jsonNumber("1234"));
-    static_assert(jsonNumber("1234").value.i == 1234);
-    static_assert(!jsonString(""));
-    static_assert(jsonString("\"test\" lol"));
-    static_assert(jsonString("\"test\" lol").value.s == "test");
-}
-
 int main() {
     chars();
     combinators();
-    json();
     std::cout << "Hello World!\n";
 }
