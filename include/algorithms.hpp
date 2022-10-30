9bd94677da6908957cd564ceffcb2e4094bed256
Gereon V <72784429+GereonV@users.noreply.github.com>
2022-10-30 20:53:03 +0100

    Calculate Levenshtein-edit-distance more generically

--- include/algorithms.hpp
+++ include/algorithms.hpp
@@ -6,34 +6,58 @@
 namespace gat::algo {
 
 	template<typename T>
-	constexpr auto & min(T const & lhs, T const & rhs) noexcept {
+	constexpr auto & min(T const & lhs, T const & rhs, auto const &... tail) noexcept {
+		if constexpr(sizeof...(tail))
+			return min(lhs, min(rhs, tail...));
 		return lhs < rhs ? lhs : rhs;
 	}
 
-	// Wagner-Fischer (row-size dependendant on rhs)
-	constexpr std::size_t levenshtein(std::string_view lhs, std::string_view rhs) noexcept {
-		auto lsize = lhs.size(), rsize = rhs.size();
-		if(!lsize)
-			return rsize;
-		if(!rsize)
-			return lsize;
-		auto size = rsize + 1;
-		auto row = new std::size_t[size];
-		for(std::size_t i{}; i < size; ++i)
-			row[i] = i;
-		for(std::size_t i{}; auto c : lhs) { // each row
-			auto corner = i; // iota first column
-			row[0] = ++i; // i = row number
-			for(std::size_t j{}; auto c2 : rhs) { // each column
-				auto up = row[j + 1]; // left = row[j]
-				row[j + 1] = c == c2 ? corner : 1 + min(row[j], min(corner, up));
+	template<typename It>
+	using value_type = decltype(**((It *) nullptr));
+
+	template<typename T> struct ref_less_struct       { using type = T; };
+	template<typename T> struct ref_less_struct<T &>  { using type = T; };
+	template<typename T> struct ref_less_struct<T &&> { using type = T; };
+	template<typename T> using ref_less = typename ref_less_struct<T>::type; // std::remove_reference_t
+
+	template<typename It, auto Deleter = [](It const &) {}>
+	struct algorithm_buffer {
+		constexpr algorithm_buffer(auto const & it) : begin{it} {}
+		template<typename T> constexpr algorithm_buffer(T && it) : begin{static_cast<T &&>(it)} {}
+		constexpr ~algorithm_buffer() { Deleter(begin); }
+		It begin;
+	};
+
+	// Wagner-Fischer
+	// [row.begin; row.begin + rhs.size()) must be valid range
+	template<typename It, auto Deleter>
+	constexpr ref_less<value_type<It>> levenshtein(std::string_view lhs, std::string_view rhs, algorithm_buffer<It, Deleter> const & row) noexcept {
+		using Int = ref_less<value_type<It>>;
+		if(!lhs.size())
+			return rhs.size();
+		if(!rhs.size())
+			return lhs.size();
+		auto it = row.begin;
+		for(std::size_t i{1}; i <= rhs.size(); ++it)
+			*it = ++i;
+		Int left;
+		for(Int i{}; i < lhs.size();) { // each row
+			auto c = lhs[i];
+			auto corner = i;
+			left = ++i;
+			for(it = row.begin; auto c2 : rhs) { // each column
+				auto up = *it;
+				*it = left = c == c2 ? corner : 1 + min(corner, left, up);
 				corner = up;
-				++j;
+				++it;
 			}
 		}
-		size = row[rsize];
-		delete[] row;
-		return size;
+		return left;
+	}
+
+	template<typename Int = std::size_t>
+	[[nodiscard]] constexpr Int levenshtein(std::string_view lhs, std::string_view rhs) noexcept {
+		return levenshtein<Int *, [](auto ptr) { delete[] ptr; }>(lhs, rhs, new Int[rhs.size()]);
 	}
 
 }
